910053a8c8dc74d1d1b42f6de727e45ec37c8640
Tkanos <Tkanos@users.noreply.github.com>
2016-10-26 15:50:35 +0200

    Add files via upload

---
+++ src/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWorlApi.Controllers
+{
+    [RoutePrefix("api/helloworld")]
+    public class HelloWorldController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "Hello, World !";
+        }
+    }
+}
